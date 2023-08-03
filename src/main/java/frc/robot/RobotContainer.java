a4cbceebb04afdc1c578e10f010d76738e750dad
Bovine Intervention <rich.sims@bovineintervention.org>
2023-08-02 20:45:46 -0400

    Added Auto framework
    preparing for drive characterization

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,15 +6,17 @@ package frc.robot;
 
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.DriveConstants.DriveModulePosition;
 import frc.robot.commands.DriveWithJoysticks;
+import frc.robot.commands.FeedForwardCharacterization;
+import frc.robot.commands.FeedForwardCharacterization.FeedForwardCharacterizationData;
 import frc.robot.commands.SpinAuto;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.drive.GyroIO;
@@ -23,6 +25,9 @@ import frc.robot.subsystems.drive.ModuleIO;
 import frc.robot.subsystems.drive.ModuleIOFalcon500;
 import frc.robot.subsystems.drive.ModuleIOSim;
 import frc.robot.subsystems.leds.CANdleSystem;
+import frc.robot.subsystems.leds.CANdleSystem.AnimationTypes;
+import frc.robot.util.Alert;
+import frc.robot.util.Alert.AlertType;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -39,10 +44,10 @@ public class RobotContainer {
   private final CommandXboxController driveController = new CommandXboxController(0);
 
   // LEDs
-  private final CANdleSystem m_candleSubsystem;
+  private final CANdleSystem candleSystem;
 
   // Dashboard inputs
-  private final LoggedDashboardChooser<Command> autoChooser = new LoggedDashboardChooser<>("Auto Choices");
+  private final LoggedDashboardChooser<AutoRoutine> autoChooser = new LoggedDashboardChooser<>("Auto Routine");
 
   // TODO: add LED and Brake switches
   // private DigitalInput brakeSwitch = new DigitalInput(DIOPorts.brakeSwitchPort);
@@ -62,7 +67,7 @@ public class RobotContainer {
           new ModuleIOFalcon500(DriveModulePosition.BACK_LEFT),
           new ModuleIOFalcon500(DriveModulePosition.BACK_RIGHT));
 
-          m_candleSubsystem = new CANdleSystem(driveController.getHID());
+          candleSystem = new CANdleSystem(driveController.getHID());
         break;
 
       // Sim robot, instantiate physics sim IO implementations
@@ -74,7 +79,7 @@ public class RobotContainer {
           new ModuleIOSim(),
           new ModuleIOSim());
 
-          m_candleSubsystem = null;
+          candleSystem = null;
         break;
 
         default:
@@ -85,7 +90,7 @@ public class RobotContainer {
               new ModuleIO() {},
               new ModuleIO() {});
 
-          m_candleSubsystem = null;
+          candleSystem = null;
     }
 
     // Instantiate missing subsystems
@@ -104,9 +109,12 @@ public class RobotContainer {
     configureSubsystems();
     
     // Set up auto routines
-    autoChooser.addDefaultOption("Do Nothing", new InstantCommand());
-    autoChooser.addOption("Spin", new SpinAuto(drive));
     configureAutos();
+
+    // Alert if in tuning mode
+    if (Constants.tuningMode) {
+      new Alert("Tuning mode active, do not use in competition.", AlertType.INFO).set(true);
+    }
   }
 
 
@@ -119,12 +127,12 @@ public class RobotContainer {
   private void configureButtonBindings() {
     if (RobotBase.isReal()) {
       // TODO: figure out how to configure buttons with CommandXboxController
-      int xButtonNum = 3;
-      int yButtonNum = 4;
-      int bButtonNum = 2;
-      new JoystickButton(driveController.getHID(), xButtonNum).whenPressed(m_candleSubsystem::setColors, m_candleSubsystem);
-      new JoystickButton(driveController.getHID(), yButtonNum).whenPressed(m_candleSubsystem::incrementAnimation, m_candleSubsystem);
-      new JoystickButton(driveController.getHID(), bButtonNum).whenPressed(m_candleSubsystem::decrementAnimation, m_candleSubsystem);
+      // int xButtonNum = 3;
+      // int yButtonNum = 4;
+      // int bButtonNum = 2;
+      // new JoystickButton(driveController.getHID(), xButtonNum).whenPressed(m_candleSubsystem::setColors, m_candleSubsystem);
+      // new JoystickButton(driveController.getHID(), yButtonNum).whenPressed(m_candleSubsystem::incrementAnimation, m_candleSubsystem);
+      // new JoystickButton(driveController.getHID(), bButtonNum).whenPressed(m_candleSubsystem::decrementAnimation, m_candleSubsystem);
     }
   }
 
@@ -143,6 +151,47 @@ public class RobotContainer {
 
 
   private void configureAutos() {
+      // Set up auto routines
+      autoChooser.addDefaultOption("Do Nothing",
+          new AutoRoutine(AutoPosition.ORIGIN, new InstantCommand()));
+
+      autoChooser.addOption("Spin", new AutoRoutine(AutoPosition.ORIGIN, new SpinAuto(drive)));
+                
+      autoChooser.addOption(
+          "Reset Odometry", new AutoRoutine(AutoPosition.ORIGIN, new InstantCommand(() -> drive.setPose(new Pose2d()))));
+
+      autoChooser.addOption(
+          "Drive Characterization",
+          new AutoRoutine(AutoPosition.ORIGIN, new FeedForwardCharacterization(
+              drive,
+              true,
+              new FeedForwardCharacterizationData("drive"),
+              drive::runCharacterizationVolts,
+              drive::getCharacterizationVelocity)));    
+  }
+
+  private static class AutoRoutine {
+    public final AutoPosition position;
+    public final Command command;
+
+    public AutoRoutine(AutoPosition position, Command command) {
+      this.position = position;
+      this.command = command;
+    }
+  }
+
+  public static enum AutoPosition {
+    ORIGIN;
+
+    public Pose2d getPose() {
+      switch (this) {
+        case ORIGIN:
+          return new Pose2d();
+        // other defined AutoPositions
+        default:
+          return new Pose2d();
+      }
+    }
   }
 
 
@@ -152,11 +201,15 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return autoChooser.get();
+    AutoRoutine routine = autoChooser.get();
+    drive.setPose(routine.position.getPose());
+    return routine.command;
   }
 
 
   public void disabledPeriodic() {
+    candleSystem.changeAnimation(AnimationTypes.Rainbow);
+
     // if (activeAutoCommand == null || !activeAutoName.equals(autoChooser.getSelected())) {
     //     activeAutoCommand = new AutoRoutines(autoChooser.getSelected(), drive, pivot, telescope, wrist, intake);
     //     activeAutoName = autoChooser.getSelected();
