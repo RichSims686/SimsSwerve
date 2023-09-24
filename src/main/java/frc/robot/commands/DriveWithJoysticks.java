0333e00951491819a3bfb087a913a1ed643a64c4
Bovine Intervention <rich.sims@bovineintervention.org>
2023-09-23 22:23:37 -0400

    Auto stash before merge of "main" and "origin/main"

--- src/main/java/frc/robot/commands/DriveWithJoysticks.java
+++ src/main/java/frc/robot/commands/DriveWithJoysticks.java
@@ -20,66 +20,64 @@ import frc.robot.subsystems.drive.SwerveJoystickInputs;
 
 public class DriveWithJoysticks extends CommandBase {
 
-  private final Drive drive;
-  private final DoubleSupplier xSupplier; // x-axis translation
-  private final DoubleSupplier ySupplier; // y-axis translation
-  private final DoubleSupplier turnSupplier; // rotation
-  private final BooleanSupplier precisionSupplier; // slow-down for precision positioning
-  private final BooleanSupplier robotRelativeOverride; // robot-relative instead of field-relative
+    private final Drive drive;
+    private final DoubleSupplier xSupplier; // x-axis translation
+    private final DoubleSupplier ySupplier; // y-axis translation
+    private final DoubleSupplier turnSupplier; // rotation
+    private final BooleanSupplier precisionSupplier; // slow-down for precision positioning
+    private final BooleanSupplier robotRelativeOverride; // robot-relative instead of field-relative
 
-  /** Creates a new DriveWithJoysticks. */
-  public DriveWithJoysticks(Drive drive, DoubleSupplier xSupplier, DoubleSupplier ySupplier, DoubleSupplier turnSupplier,
-                            BooleanSupplier robotRelativeOverride, BooleanSupplier precisionSupplier) {
-    addRequirements(drive);
-    this.drive = drive;
-    this.xSupplier = xSupplier;
-    this.ySupplier = ySupplier;
-    this.turnSupplier = turnSupplier;
-    this.robotRelativeOverride = robotRelativeOverride;
-    this.precisionSupplier = precisionSupplier;
-  }
-
-  @Override
-  public void initialize() {
-  }
-
-  @Override
-  public void execute() {
-
-    boolean squareInputs = true;
-
-    // process joystick inputs
-    SwerveJoystickInputs inputs = new SwerveJoystickInputs(xSupplier.getAsDouble(), 
-                                               ySupplier.getAsDouble(),
-                                               turnSupplier.getAsDouble(),
-                                               squareInputs,
-                                               squareInputs,
-                                               precisionSupplier.getAsBoolean());
-
-    // Convert to meters/sec and radians/sec
-    double vxMetersPerSecond = inputs.getX() * drive.getMaxLinearSpeedMetersPerSec();
-    double vyMetersPerSecond = inputs.getY() * drive.getMaxLinearSpeedMetersPerSec();
-    double omegaRadiansPerSecond = inputs.getTurn() * drive.getMaxAngularSpeedRadiansPerSec();
-
-    // field relative controls
-    ChassisSpeeds speeds = new ChassisSpeeds(vxMetersPerSecond, vyMetersPerSecond, omegaRadiansPerSecond);
-
-    if (robotRelativeOverride.getAsBoolean()) {
-      // robot relative controls
-      var driveRotation = drive.getRotation(); // angle from alliance wall normal
-      if (DriverStation.getAlliance() == Alliance.Red) {
-        driveRotation = driveRotation.rotateBy(new Rotation2d(Math.PI));
-      }
-      speeds = ChassisSpeeds.fromFieldRelativeSpeeds(speeds, driveRotation);
+    /** Creates a new DriveWithJoysticks. */
+    public DriveWithJoysticks(Drive drive, DoubleSupplier xSupplier, DoubleSupplier ySupplier,
+            DoubleSupplier turnSupplier, BooleanSupplier robotRelativeOverride, BooleanSupplier precisionSupplier) {
+        addRequirements(drive);
+        this.drive = drive;
+        this.xSupplier = xSupplier;
+        this.ySupplier = ySupplier;
+        this.turnSupplier = turnSupplier;
+        this.robotRelativeOverride = robotRelativeOverride;
+        this.precisionSupplier = precisionSupplier;
     }
 
-    drive.driveVelocity(speeds);
-  }
+    @Override
+    public void initialize() {
+    }
 
-  @Override
-  public void end(boolean interrupted) {
-    drive.stop();
-  }
+    @Override
+    public void execute() {
 
+        boolean squareInputs = true;
+
+        // process joystick inputs
+        SwerveJoystickInputs inputs = new SwerveJoystickInputs(xSupplier.getAsDouble(),
+                ySupplier.getAsDouble(),
+                turnSupplier.getAsDouble(),
+                squareInputs,
+                precisionSupplier.getAsBoolean());
+
+        // Convert to meters/sec and radians/sec
+        double vxMetersPerSecond = inputs.getX() * drive.getMaxLinearSpeedMetersPerSec();
+        double vyMetersPerSecond = inputs.getY() * drive.getMaxLinearSpeedMetersPerSec();
+        double omegaRadiansPerSecond = inputs.getTurn() * drive.getMaxAngularSpeedRadiansPerSec();
+
+        // field relative controls
+        ChassisSpeeds speeds = new ChassisSpeeds(vxMetersPerSecond, vyMetersPerSecond, omegaRadiansPerSecond);
+
+        if (robotRelativeOverride.getAsBoolean()) {
+            // robot relative controls
+            var driveRotation = drive.getRotation(); // angle from alliance wall normal
+            if (DriverStation.getAlliance() == Alliance.Red) {
+                driveRotation = driveRotation.rotateBy(new Rotation2d(Math.PI));
+            }
+            speeds = ChassisSpeeds.fromFieldRelativeSpeeds(speeds, driveRotation);
+        }
+
+        drive.driveVelocity(speeds);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drive.stop();
+    }
 
 }
